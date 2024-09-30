3f808e74570a7a814ed9ab1ccaacfcf90cc358a1
lionard <leonard.muhati@student.moringaschool.com>
2024-09-30 15:22:32 +0300

    compelete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,17 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+    def __init__(self, name="unknown", breed="unknown"):
+        self.name=name
+        self.breed = breed
+        if not isinstance(name, str) or len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            return
+
+        if not isinstance(breed, str) or breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            return
+
+        self.name = name
+        self.breed = breed
